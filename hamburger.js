3cbf9fff4483a42b7c66efb4807590cc7aae4cdb
Michael Harris <mhguitar1997@gmail.com>
2019-05-24 09:44:57 -0700

    Changed header party planner font and background image

--- hamburger.js
+++ hamburger.js
@@ -1,6 +1,8 @@
+const header = document.querySelector('header');
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const main = document.querySelector("main");
+const sticky = header.offsetTop;
 
 
 
@@ -9,3 +11,12 @@ hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("show-nav");
 });
 
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  }
+
+window.addEventListener('scroll', myFunction);
